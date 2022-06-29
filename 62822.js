d5eda8b3ee6bfac4bf2aeb91e2d716b1025eecf5
brookebytes <brookeared@gmail.com>
2022-06-28 21:49:44 -0400

    june 28th kata

---
+++ 62822.js
@@ -0,0 +1,8 @@
+//The highest profit wins! - 7kyu
+/*Write a function that returns both the minimum and maximum number of 
+* the given list/array.*/
+
+function minMax(arr){
+  arr.sort((a,b) => a-b);
+  return [arr[0],arr[arr.length-1]];
+}
