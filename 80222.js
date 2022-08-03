643da501f063ff7c720d891d5b05967f14d8c164
brookebytes <brookeared@gmail.com>
2022-08-02 23:27:00 -0400

    august 2nd kata

---
+++ 80222.js
@@ -0,0 +1,6 @@
+//Beginner Series #4 Cockroach - 8kyu
+//Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+function cockroachSpeed(s) {
+  return Math.floor((s*100000)/3600);
+}
