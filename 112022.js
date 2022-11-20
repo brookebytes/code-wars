38dc4d7b44e1f9d069eecc60f86d87b598ef82b1
Brooke <brookeared@gmail.com>
2022-11-20 14:59:22 -0500

    november 20th kata+1

--- 112022.js
+++ 112022.js
@@ -1,4 +1,14 @@
 //Unscrambled eggs - 7kyu
-//Return the string without the word "egg" in it
+// Return the string without the word "egg" in it
 
 const unscrambleEggs = word => word.split("egg").filter(x => x!=="egg").join("");
+
+//Anagram Detection - 7kyu
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise
+
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  return test.toUpperCase().split("").sort().join("") == original.toUpperCase().split("").sort().join("");
+
+};
+
