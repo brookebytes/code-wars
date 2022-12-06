b21a5892404ef6b12c7360f5b0bd501cf8f53923
Brooke <brookeared@gmail.com>
2022-12-06 00:03:36 -0500

    december 6th kata

---
+++ 120622.js
@@ -0,0 +1,8 @@
+//Enumberable Magic - Does My List Include This? - 8kyu
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+function include(arr, item){
+  return arr.includes(item);
+}
