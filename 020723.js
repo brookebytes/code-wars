666a81937097be3116c91d4159a4b03465ecd506
Brooke <brookeared@gmail.com>
2023-02-07 22:57:18 -0500

    february 7th 2023 kata

---
+++ 020723.js
@@ -0,0 +1,11 @@
+//Sum of Cubes - 7kyu
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+// Assume that the input n will always be a positive integer.
+
+function sumCubes(n){
+  let sum = 0;
+  for (i=1; i<=n; i++){
+    sum+=(i**3;
+  }
+  return sum;
+}
