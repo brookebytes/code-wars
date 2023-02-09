46371ba44c7e15a39d47dbfe3433b834140dc6a8
Brooke <brookeared@gmail.com>
2023-02-09 16:40:24 -0500

    february 9th kata

---
+++ 020923.js
@@ -0,0 +1,4 @@
+//Convert an array of strings to array of numbers - 7kyu
+//Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+toNumberArray = stringarray => stringarray.map(str => Number(str));
