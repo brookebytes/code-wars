412360d3f0bc92e39339f12d3f48f211592b3c33
Brooke <brookeared@gmail.com>
2023-02-15 16:19:44 -0500

    february 15th 2023 kata

---
+++ 021523.js
@@ -0,0 +1,4 @@
+//Find Nearest square number - 8kyu
+// Your task is to find the nearest square number of a positive integer. If the n is already the perfect square you need to just return n.
+
+const nearestSq = n => Math.round(Math.sqrt(n))**2;
