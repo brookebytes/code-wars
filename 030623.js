7241d430096b5a4acd27e061d10677e224a2025f
Brooke <brookeared@gmail.com>
2023-03-06 13:35:17 -0500

    march 6th 2023 kata

---
+++ 030623.js
@@ -0,0 +1,8 @@
+//Classy Extentions - 7kyu
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows.
+
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`
+  }
+}
