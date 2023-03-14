bdc54bb06af39dd73296f567406508c183bb1bf1
Brooke <brookeared@gmail.com>
2023-03-14 15:10:42 -0400

    moved 2023 kata to proper folder

--- 2022/012023.js
+++
@@ -1,6 +0,0 @@
-//Sort array by string length - 7kyu
-
-function sortByLength (array) {
-  // Return an array containing the same strings, ordered from shortest to longest
-  return array.sort((a,b) => a.length-b.length);
-};
