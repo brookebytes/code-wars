15d197aacc04d862efedd6f3ae65caca76865b6a
Brooke <brookeared@gmail.com>
2023-04-10 14:27:16 -0400

    april 10th 2023 kata

---
+++ 2023/041023.js
@@ -0,0 +1,7 @@
+//Between Extremes - 7kyu
+/* Given an array of numbers, return the difference between the largest and smallest values.
+ * The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow. */
+
+function betweenExtremes(numbers) {
+    return Math.max(...numbers) - Math.min(...numbers);
+}
