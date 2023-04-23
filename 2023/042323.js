bf0472d48d3a9aba14666b456f7ded615b703429
Brooke <brookeared@gmail.com>
2023-04-23 17:38:01 -0400

    april 23rd 2023 kata

---
+++ 2023/042323.js
@@ -0,0 +1,7 @@
+//Transportation on vacation - 8kyu
+/* Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+ * Write a code that gives out the total amount for different days(d). */
+
+function rentalCarCost(d) {
+  return d < 3 ? d*40 : d >= 7 ? (d*40)-50 : (d*40)-20;
+}
