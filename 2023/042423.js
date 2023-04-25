6d2031f71fef70edc89dd31ae2c0cf0f5305f92e
Brooke <brookeared@gmail.com>
2023-04-24 22:43:27 -0400

    april 24th 2023 kata

---
+++ 2023/042423.js
@@ -0,0 +1,8 @@
+//Enumberable Magic #25 - Take the First N Elements - 8kyu
+/* Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array. */
+
+function take(arr, n) {
+  return arr.slice(0,n)
+}
+
+
