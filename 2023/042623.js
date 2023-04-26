1a8b038d816c7e020e4405cf0e02dbfc5c301b2c
Brooke <brookeared@gmail.com>
2023-04-26 11:44:29 -0400

    april 26th 2023 kata

---
+++ 2023/042623.js
@@ -0,0 +1,8 @@
+//Flatten and sort an array - 7kyu
+//Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+"use strict";
+
+function flattenAndSort(array) {
+  return array.flat().sort((a,b) => a-b);
+}
