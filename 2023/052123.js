7b1df26c421ae2e521ea687a15f4c652d41d5544
Brooke <brookeared@gmail.com>
2023-05-21 20:36:37 -0400

    may 21st 2023 kata

---
+++ 2023/052123.js
@@ -0,0 +1,6 @@
+//Remove anchor from URL - 7kyu
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+function removeUrlAnchor(url){
+  return url.includes('#') ? url.slice(0,url.indexOf('#')) : url;
+}
