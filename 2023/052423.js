d66fd4705f3fdb3b9f8ab2cef46239b42261389c
Brooke <brookeared@gmail.com>
2023-05-24 14:47:29 -0400

    may 24th 2023 kata

---
+++ 2023/052423.js
@@ -0,0 +1,6 @@
+//BASIC: Making Six Toast. - 8kyu
+// Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative.
+
+function sixToast(num) {
+  return Math.abs(6-num);
+}
