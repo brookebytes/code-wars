0b102856c9935350d38c311bff053a4a1d8b9855
Brooke <brookeared@gmail.com>
2023-05-25 18:28:36 -0400

    may 25th 2023 kata

---
+++ 2023/052523.js
@@ -0,0 +1,6 @@
+//Basic Mathematical Operations - 8kyu
+// Your task is to create a function that does four basic mathematical operations.
+
+function basicOp(operation, value1, value2) {
+  return eval(value1 + operation + value2);
+}
