cad55b377a47db4149fb81699790c84b6d5a5242
Brooke <brookeared@gmail.com>
2023-05-26 14:55:06 -0400

    may 26th 2023 kata

---
+++ 2023/052623.js
@@ -0,0 +1,6 @@
+//Fake Binary - 8kyu
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+  return x.split("").map(num => parseInt(num) >= 5 ? 1 : 0).join("");
+}
