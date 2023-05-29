b15fff66364aeb5e4b833234715524b9b21d4287
Brooke <brookeared@gmail.com>
2023-05-29 13:13:36 -0400

    may 29th 2023 katA

---
+++ 2023/052923.js
@@ -0,0 +1,4 @@
+//99 Problems, #1: last in list - 7kyu
+// Write a function last that accepts a list and returns the last element in the list.
+
+const last = xs => xs.length>0 ? xs[xs.length-1] : null;
