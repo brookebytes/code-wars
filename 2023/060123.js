32f20f7b7bf0db11aa9c890582549566e8ad6f40
Brooke <brookeared@gmail.com>
2023-06-01 15:22:07 -0400

    june 1st 2023 kata

---
+++ 2023/060123.js
@@ -0,0 +1,5 @@
+//ES6 string addition - 8kyu
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
+const joinStrings = (string1, string2) => `${string1} ${string2}`;
+
