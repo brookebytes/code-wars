9b654c5c618029d81a4685a986ca0deb5a8eaeea
Brooke <brookeared@gmail.com>
2023-06-09 17:38:42 -0400

    june 9th 2023 kata

---
+++ 2023/060923.js
@@ -0,0 +1,7 @@
+//Hello World - Without Strings - 7kyu
+/* You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions. */
+
+
+const helloWorld = () => {
+  return String.fromCharCode(72,101,108,108,111,44,32) + String.fromCharCode(87,111,114,108,100,33);
+};
