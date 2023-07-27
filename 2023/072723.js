ab03a01bacbe216fc5498282ee8566075aa9baf3
Brooke <brookeared@gmail.com>
2023-07-27 17:20:24 -0400

    july 27th 2023 kata

---
+++ 2023/072723.js
@@ -0,0 +1,6 @@
+//Training JS #18: Methods of String object - 8kyu
+/* The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces. */
+
+function splitAndMerge(string, separator) {
+  return string.split(" ").map(el => el.split("").join(separator)).join(" ");
+}
