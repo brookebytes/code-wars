14f7de4906fc621f41116f428c51650836aab2b2
Brooke <brookeared@gmail.com>
2023-08-07 12:25:38 -0400

    august 7th 2023 kata

---
+++ 2023/080723.js
@@ -0,0 +1,6 @@
+//Thinkful - String Drills: Quotable - 7kyu
+// This function should take two string parameters: a person's name (name) and a quote of theirs (quote), and return a string attributing the quote to the person
+
+function quotable(name, quote){
+  return `${name} said: "${quote}"`
+}
