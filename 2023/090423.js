84e661415a91724d508c99d4f1e8fec8d00904b8
Brooke <brookeared@gmail.com>
2023-09-04 19:11:14 -0400

    september 4th 2023 kata

---
+++ 2023/090423.js
@@ -0,0 +1,7 @@
+//Surface Area and Volume of a Box - 8kyu
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth) {
+  return [ ( 2*(width*height + height*depth + width*depth) ) , ( width*height*depth ) ]
+}
+
