b84ec5926eee5228ccb8d41c4a0945f05078aead
Brooke <brookeared@gmail.com>
2024-12-04 16:45:00 -0500

    december 4th 2024 kata

---
+++ 2024/120424.js
@@ -0,0 +1,7 @@
+//Char Code Calculation - 7kyu
+// You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+var ArrowFunc = function(arr) {
+  return arr.map( e => String.fromCharCode(e)).join(''); //Complete this function
+}
+
