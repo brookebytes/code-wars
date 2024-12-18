5da32e6304f8b728ed98e7c32da2cdbdb091cb4b
Brooke <brookeared@gmail.com>
2024-12-18 18:27:55 -0500

    december 18th 2024 kata

---
+++ 2024/121824.js
@@ -0,0 +1,26 @@
+// ToLeetSpeak - 7kyu
+// Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+// Consider only uppercase letters and spaces.
+
+function toLeetSpeak(str) {
+  dict = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+
